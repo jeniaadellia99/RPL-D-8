7581e0450e7a092eb7b88fd73b46b6e85378ea32
beykun <bayurasukmaraga@gmail.com>
2018-03-21 16:11:14 +0700

    hapus testlara

--- testlara/resources/views/admin/layout/app_edit.blade.php
+++
@@ -1,1 +0,0 @@
-@include('admin.patisi.footer_edit')
--- testlara/resources/views/admin/partisi/footer_edit.blade.php
+++
@@ -1,9 +0,0 @@
- <div class="modal-footer">
- 	<button class="btn btn-success" type="submit">Simpan</button>
- 	<button class="btn btn-default" data-dismiss="modal" aria-hidden="true">Keluar</button>
- </div>
-</form>
-</div>
-</div>
-</div>
-</div>
